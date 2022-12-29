36c6c795e432d165d38c1e12f6ab880a881029a7
Y_Less <git-athena@y-less.com>
2022-12-29 03:34:53 +0000

    Add docker files.

---
+++ docker/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Available configs: Debug, [RelWithDebInfo], Release
+[[ -z "$CONFIG" ]] \
+&& config=RelWithDebInfo \
+|| config="$CONFIG"
+# Available versions: 18.04, [20.04], 22.04
+[[ -z "$UBUNTU_VERSION" ]] \
+&& ubuntu_version=20.04 \
+|| ubuntu_version="$UBUNTU_VERSION"
+# Available options: [true], false
+[[ -z "$BUILD_SHARED" ]] \
+&& build_shared=1 \
+|| build_shared="$BUILD_SHARED"
+# Available options: [true], false
+[[ -z "$BUILD_SERVER" ]] \
+&& build_server=1 \
+|| build_server="$BUILD_SERVER"
+# Available options: true, [false]
+[[ -z "$BUILD_TOOLS" ]] \
+&& build_tools=0 \
+|| build_tools="$BUILD_TOOLS"
+
+docker build \
+    -t open.mp/build:ubuntu-${ubuntu_version} \
+    build_ubuntu-${ubuntu_version}/ \
+|| exit 1
+
+folders=('build' 'conan')
+for folder in "${folders[@]}"; do
+    if [[ ! -d "./${folder}" ]]; then
+        mkdir ${folder} &&
+        chown 1000:1000 ${folder} || exit 1
+    fi
+done
+
+docker run \
+    --rm \
+    -t \
+    -w /code \
+    -v $PWD/..:/code \
+    -v $PWD/build:/code/build \
+    -v $PWD/conan:/home/user/.conan \
+    -e CONFIG=${config} \
+    -e BUILD_SHARED=${build_shared} \
+    -e BUILD_SERVER=${build_server} \
+    -e BUILD_TOOLS=${build_tools} \
+    -e OMP_BUILD_VERSION=$(git rev-list $(git rev-list --max-parents=0 HEAD) HEAD | wc -l) \
+    -e OMP_BUILD_COMMIT=$(git rev-parse HEAD) \
+    open.mp/build:ubuntu-${ubuntu_version}
